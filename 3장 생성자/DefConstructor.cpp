833ae616c056406e0368f789fe153fac36028f71
ochestra365 <ochestra365@naver.com>
2021-05-31 14:49:48 +0900

    객체 디폴트하기

---
+++ 3장 생성자/DefConstructor.cpp
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//
+//class Human {
+//private:
+//	char name[12];
+//	int age;
+//public:
+//	Human() {
+//		strcpy(name, "이름없음");
+//		age = 0;
+//	}
+//	void intro() {
+//		printf("이름 = %s, 나이 %d\n", name, age);
+//	}
+//};
+//int main() {
+//	Human momo;//모모가 초기화 되지 않았으므로 디폴트값이 튀어 나온다.
+//	momo.intro();
+//}
---
+++ 3장 생성자/Destructor.cpp
@@ -0,0 +1,28 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//
+//class Human {
+//private:
+//	char* pname;
+//	int age;
+//
+//public:
+//	Human(const char* aname, int aage) {
+//		pname = new char[strlen(aname) + 1];
+//		strcpy(pname, aname);
+//		age = aage;
+//		printf("%s 객체가 생성자가 호출되었습니다.\n", pname);
+//	}
+//	~Human() {
+//		printf("%s 객체가 파괴되었습니다.\n", pname);
+//		delete[]pname;
+//	}
+//	void intro() {
+//		printf("이름 = %s, 나이 = %d\n", pname, age);
+//	}
+//};
+//Human boy("김수한무거북이와두루미", 12);
+//int main() {
+//	boy.intro();
+//}
