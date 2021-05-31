20a956c7564874a649d6c70d32f00b2dad543f09
ochestra365 <ochestra365@naver.com>
2021-05-31 09:21:44 +0900

    overloading

--- Project1/Overload.cpp
+++ Project1/Overload.cpp
@@ -1,21 +1,21 @@
-#include <stdio.h>
-
-int Add(int a, int b);
-int Add(int a, int b, int c);
-double Add(double a, double b);
-
-int main() {
-	printf("%d\t", Add(1, 2));
-	printf("%d\t", Add(1, 2, 3));
-	printf("%.1lf", Add(1.2, 1.5));
-	return 0;
-}
-int Add(int a, int b) {
-	return a + b;
-}
-int Add(int a, int b, int c) {
-	return a + b + c;
-}
-double Add(double a, double b) {
-	return a + b;
-}
+//#include <stdio.h>
+//
+//int Add(int a, int b);
+//int Add(int a, int b, int c);
+//double Add(double a, double b);
+//
+//int main() {
+//	printf("%d\t", Add(1, 2));
+//	printf("%d\t", Add(1, 2, 3));
+//	printf("%.1lf", Add(1.2, 1.5));
+//	return 0;
+//}
+//int Add(int a, int b) {
+//	return a + b;
+//}
+//int Add(int a, int b, int c) {
+//	return a + b + c;
+//}
+//double Add(double a, double b) {
+//	return a + b;
+//}
