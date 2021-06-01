f8f02a2d26a7fbd56efd92c2b41af5eca504c754
ochestra365 <ochestra365@naver.com>
2021-06-01 15:50:48 +0900

    주석

---
+++ 4장 캡슐화/FriendClass.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+class Time {
+	friend class Date;//데이트 클래스의 모든 멤버변수를 참조한다.
+private:
+	int hour, min, sec;//타임 클래스는 시간과 관련된 멤버변수를 가지고 있다.
+public:
+	Time(int h, int m, int s) { hour = h; min = m; sec = s; }//메인에서 복사한 값을 멤버변수로 설정한다. 생성자가 만들어졌다.
+};
+class Date {//데이트 클래스가 나와 있다. (클래스는 가장 좋은 식별자가 해당 객체를 직관적으로 잘 알 수 있게 하는 것이다.
+private:
+	int year, month, day;//데이트 클래스 내에서만 핸들링 할 수 있도록 프라이빗 접근제한자를 설정한다. 데이터의 무결성
+public:
+	Date(int y, int m, int d) { year = y; month = m; day = d; }//메인에서 복사한 값을 넘겨준다. 생성자가 만들어졌다.
+	void OutToday(Time& t) {//만들어진 타임 클래스의 레퍼런스를 인수로 사용한다. 타임 클래스의 객체를 직접 이용하는 것은 타임 객체를 위협하기 때문이다. 기능 함수
+		printf("오늘은 %d년 %d월 %d일이며 지금 시간은 %d:%d:%d입니다.\n",//딱히 중요한 건 아니고
+			year,month, day, t.hour, t.min, t.sec);//데이트 클래스의 멤버변수는 물론이거니와 타임 클래스의 멤버 변수까지 모두 사용할 수 있다.
+	}
+};
+int main() {//항상 메인을 가장 먼저 봐야 한다.
+	Date d(2018, 06, 29);//이후 데이트 클래스를 봐야 한다.
+	Time t(12, 34, 56);//이후 타임 클래스를 봐야한다.
+	d.OutToday(t);
+	return 0;
+}
--- 4장 캡슐화/Self99page.cpp
+++ 4장 캡슐화/Self99page.cpp
@@ -1,64 +1,64 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <conio.h>
-#include "cursor.h"
-
-class Human {
-private:
-	char* pname;
-	int age;
-public:
-	Human(const char* aname, int aage) {
-		pname = new char[strlen(aname) + 1];
-		strcpy(pname, aname);
-		age = aage;
-	}
-	Human(const Human& other) {
-		pname = new char[strlen(other.pname) + 1];
-		strcpy(pname, other.pname);
-		age = other.age;
-	}
-	~Human() {
-		delete[] pname;
-		printf("생성자 소멸\n");
-	}
-	void intro() {
-		if ((strlen(pname) < 12) && (age > 0) && (age < 120)) {
-			printf("이름 = %s, 나이 =%d\n", pname, age);
-		}
-		else {
-			printf("이름은 12자 미만, 나이는 0~120까지 입력가능합니다.");
-			exit(1);
-		}
-
-	}
-};
-void PrintHuman(Human who) {
-	who.intro();
-}
-void print_line() {
-	printf("\n");
-	for (int i = 0; i < 50; i++) {
-		printf("=");
-	}
-	printf("\n");
-}
-int main() {
-	printf("이름과 나이를 적으세요\n(단, 이름은 12자 미만, 나이는 0~120까지 입력가능\n");
-	print_line();
-	while (1) {
-		char name[12];
-		int age;
-		printf(">이름 : ");
-		scanf("%s", name);
-		getchar();//개행 버퍼 없애줌
-		printf(">나이 : ");
-		scanf("%d", &age);
-		Human park(name, age);
-		PrintHuman(park);
-	}
-	
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <conio.h>
+//#include "cursor.h"
+//
+//class Human {
+//private:
+//	char* pname;
+//	int age;
+//public:
+//	Human(const char* aname, int aage) {
+//		pname = new char[strlen(aname) + 1];
+//		strcpy(pname, aname);
+//		age = aage;
+//	}
+//	Human(const Human& other) {
+//		pname = new char[strlen(other.pname) + 1];
+//		strcpy(pname, other.pname);
+//		age = other.age;
+//	}
+//	~Human() {
+//		delete[] pname;
+//		printf("생성자 소멸\n");
+//	}
+//	void intro() {
+//		if ((strlen(pname) < 12) && (age > 0) && (age < 120)) {
+//			printf("이름 = %s, 나이 =%d\n", pname, age);
+//		}
+//		else {
+//			printf("이름은 12자 미만, 나이는 0~120까지 입력가능합니다.");
+//			exit(1);
+//		}
+//
+//	}
+//};
+//void PrintHuman(Human who) {
+//	who.intro();
+//}
+//void print_line() {
+//	printf("\n");
+//	for (int i = 0; i < 50; i++) {
+//		printf("=");
+//	}
+//	printf("\n");
+//}
+//int main() {
+//	printf("이름과 나이를 적으세요\n(단, 이름은 12자 미만, 나이는 0~120까지 입력가능\n");
+//	print_line();
+//	while (1) {
+//		char name[12];
+//		int age;
+//		printf(">이름 : ");
+//		scanf("%s", name);
+//		getchar();//개행 버퍼 없애줌
+//		printf(">나이 : ");
+//		scanf("%d", &age);
+//		Human park(name, age);
+//		PrintHuman(park);
+//	}
+//	
+//	return 0;
+//}
